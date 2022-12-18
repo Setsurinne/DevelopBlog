296a9fed404d5408e93ff31ba8b8e1b31109731a
Jason <63473855+Setsurinne@users.noreply.github.com>
2022-12-18 20:29:28 +0800

    EnumComp

---
+++ Assets/Sample/EnumComp/EnumComp.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using Sample;
+using UnityEngine;
+
+public enum MyEnum
+{
+    DummyEnum,
+    NotDummyEnum
+}
+public class EnumComp : TesterBase
+{
+    public Dictionary<MyEnum, int> DummyDict { get; set; }
+
+    public override void DoTest()
+    {
+        TestDefaultComp();
+        TestGivenComp();
+        Debug.LogFormat("EnumComp completed!");    
+    }
+
+    private void TestDefaultComp()
+    {
+        DummyDict = new();
+        DictOperation();
+
+    }
+
+    private void TestGivenComp()
+    {
+        DummyDict = new(new Dictionary<MyEnum, int>(new MyEnumComparer()));
+        DictOperation();
+    }
+
+    private void DictOperation()
+    {
+        if (!DummyDict.ContainsKey(MyEnum.DummyEnum))
+        {
+            DummyDict.Add(MyEnum.DummyEnum, 0);
+        }
+        if (!DummyDict.ContainsKey(MyEnum.NotDummyEnum))
+        {
+            DummyDict.Add(MyEnum.NotDummyEnum, 1);
+        }
+        DummyDict[MyEnum.DummyEnum] = DummyDict[MyEnum.NotDummyEnum];
+    }
+}
+
+public class MyEnumComparer : IEqualityComparer<MyEnum>
+{
+    public bool Equals(MyEnum x, MyEnum y)
+    {
+        return (int)x == (int)y;
+    }
+
+    public int GetHashCode(MyEnum obj)
+    {
+        return (int)obj;
+    }
+}
---
+++ Assets/Sample/TesterBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace Sample
+{
+    public class TesterBase : MonoBehaviour
+    {
+        public string sampleId = "tester";
+        public KeyCode testKey = KeyCode.A;
+        public float pauseWait = 0;
+        
+        private void Update()
+        {
+            if (Input.GetKeyUp(testKey))
+            {
+                RunTest();
+            }
+        }
+
+        [ContextMenu("RunTest")]
+        public void RunTest()
+        {
+            //Profiler.BeginSample(sampleId);
+            DoTest();
+            StartCoroutine(StopApplication());
+            //Profiler.EndSample();
+        }
+
+        public virtual void DoTest()
+        {
+            
+        }
+
+        protected virtual IEnumerator StopApplication()
+        {
+            yield return new WaitForSeconds(pauseWait);
+            EditorApplication.isPaused = true;
+        }
+    }
+}
